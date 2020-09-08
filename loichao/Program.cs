dc2f60952c099a1bb1e79d9c7f6529b493ce0d3c
tuananh200599 <anh453138@gmail.com>
2020-09-08 11:52:50 +0700

    Hello

---
+++ loichao/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace loichao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String a;
+            Console.Write("Enter your name: ");
+            a = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Hello:"+a);
+            Console.ReadKey();
+        }
+
+    }
+}
