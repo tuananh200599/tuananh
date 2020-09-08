d46f688458616d4ec7252246189fcb4463c96539
tuananh200599 <anh453138@gmail.com>
2020-09-08 09:31:30 +0700

    Hello

---
+++ bai1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bai1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Ng Tuan Anh!");
+        }
+    }
+}
