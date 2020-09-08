cc648f24ff7d7bcfe2284a5e03dc9e74c7aa876f
tuananh200599 <anh453138@gmail.com>
2020-09-08 11:02:07 +0700

    Hello

---
+++ dtichhcn/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace dtichhcn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\n");
+            Console.Write("Tinh dien tich HCN:\n");
+            Console.Write("\n\n");
+
+            float cd;
+            float cr;
+
+            Console.Write("Nhap vao chieu dai HCN:\n ");
+            cd = float.Parse(Console.ReadLine());
+            Console.Write("Nhap vao chieu rong HCN:\n ");
+            cr = float.Parse(Console.ReadLine());
+
+            float dientich = cd * cr;
+            Console.WriteLine("Dien tich HCN la: " + dientich);
+
+        }
+    }
+}
