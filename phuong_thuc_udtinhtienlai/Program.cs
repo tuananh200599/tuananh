1f817b50e81f11ea80877b60190663492866e5e2
tuananh200599 <anh453138@gmail.com>
2020-09-09 22:08:37 +0700

    Hello

---
+++ phuong_thuc_udtinhtienlai/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace phuong_thuc_udtinhtienlai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("UNG DUNG TINH TIEN LAI ");
+            Console.WriteLine("----------------------------------------- ");
+
+            double money = 1.0;
+            int month = 1;
+            double intersetRate = 1.0;
+
+            Console.WriteLine("Nhap so tien gui : ");
+            money = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nhap so thang gui : ");
+            month = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nhap % lai suat: ");
+            intersetRate = Double.Parse(Console.ReadLine());
+
+            TinhTienLai(money,month,intersetRate);
+            
+        }
+        static void TinhTienLai(double money, int month, double intersetRate)
+        {
+
+            double totalInterset = 0;
+            for (int i = 0; i < month; i++)
+            {
+                totalInterset = money * (intersetRate / 100) / 12 * 3;
+            }
+
+            Console.WriteLine("Lai suat cua ban trong {0} thang la : {1}", month, totalInterset);
+            Console.ReadKey();
+        }
+    }
+}
