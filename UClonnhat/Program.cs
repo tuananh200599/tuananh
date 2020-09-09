2f8e1e5cdd1cfb97dfa0671caefd544bfe295518
tuananh200599 <anh453138@gmail.com>
2020-09-09 11:02:37 +0700

    Hello

---
+++ UClonnhat/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace UClonnhat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            do
+            {
+                Console.WriteLine("Nhap gia tri so a: ");
+                a = int.Parse(Console.ReadLine());
+                Console.WriteLine("Nhap gia tri so b: ");
+                b = int.Parse(Console.ReadLine());
+            } while (a == 0 || b == 0);
+            Math.Abs(a);
+            Math.Abs(b);
+            while (a != b)
+            {
+                if (a > b) 
+                    a = a - b;
+                else b = b - a;
+            }
+            Console.WriteLine("Uoc so chung lon nhat la: {0}", a);
+            Console.ReadLine();
+        }
+
+    }
+}
+
