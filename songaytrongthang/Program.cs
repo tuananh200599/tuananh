a1d9024058f534b0605969fb8caca880f4bde47e
tuananh200599 <anh453138@gmail.com>
2020-09-09 11:15:09 +0700

    Hello

---
+++ songaytrongthang/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace songaytrongthang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t, n;
+            Console.WriteLine("Hay nhap thang: ");
+            t = int.Parse(Console.ReadLine());
+            if (t <= 1 || t <= 12)
+            {
+                switch (t)
+                {
+                    case 1:
+                    case 3:
+                    case 5:
+                    case 7:
+                    case 8:
+                    case 10:
+                    case 12:
+                        Console.WriteLine("Thang {0} co 31 ngay", t); break;
+                    case 4:
+                    case 6:
+                    case 9:
+                    case 11:
+                        Console.WriteLine("Thang {0} co 30 ngay", t); break;
+                    case 2:
+                        Console.WriteLine("Moi nhap them nam: ");
+                        n = int.Parse(Console.ReadLine());
+                        if ((n % 4 == 0 && n % 100 != 0) || n % 400 == 0)
+                        Console.WriteLine("Thang {0} co 29 ngay", t);
+                        else Console.WriteLine("Thang {0} co 28 ngay", t); break;
+                }
+            }
+            else Console.WriteLine("Khong ton tai thang nay");
+        }
+    }
+}
