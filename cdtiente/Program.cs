706bcb8551f5d383f91b4e512f16a1f6e509bd63
tuananh200599 <anh453138@gmail.com>
2020-09-09 10:55:47 +0700

    Hello

---
+++ cdtiente/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace cdtiente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap ti gia tien:");
+            int tigia = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap USD :");
+            int usd = int.Parse(Console.ReadLine());
+            int vnd = tigia * usd;
+            Console.WriteLine("VND chuyen doi la : {0} VND", vnd);
+        }
+    }
+}
