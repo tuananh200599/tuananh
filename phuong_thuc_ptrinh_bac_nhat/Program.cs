46df6be828ef521d93866b2303d3225f3e029545
tuananh200599 <anh453138@gmail.com>
2020-09-09 18:13:15 +0700

    Hello

---
+++ phuong_thuc_ptrinh_bac_nhat/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace phuong_thuc_ptrinh_bac_nhat{
+class Program
+    {
+
+        static void Giaiphuongtrinh()
+        {
+            double a;
+            double b;
+            Console.WriteLine("a: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            if (a != 0)
+            {
+                double solution = -b / a;
+                Console.Write("The solution is: {0}!", solution);
+            }
+            else
+            {
+                if (b == 0)
+                {
+                    Console.Write("The solution is all x!");
+                }
+                else
+                {
+                    Console.Write("Nod solution!");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Giaiphuongtrinh();
+            Console.ReadKey();
+        }
+    }
+}
