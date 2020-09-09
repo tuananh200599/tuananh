f4989383b565f4e31e1b68fe55332a562013a8a2
tuananh200599 <anh453138@gmail.com>
2020-09-09 22:19:11 +0700

    Hello

---
+++ phuong_thuc_dthinhchunhat/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace phuong_thuc_dthinhchunhat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float width;
+            float height;
+            Console.Write("Enter width: ");
+            width = float.Parse(Console.ReadLine());
+            Console.Write("Enter height: ");
+            height = float.Parse(Console.ReadLine());
+            float area = width * height;
+            Console.WriteLine("Area is: " + area);
+
+            //float area = width * height;
+            dientich(width, height);
+            Console.ReadKey();
+        }
+
+        static float dientich(float a,float b)
+        {
+            float c = a*b;
+            Console.WriteLine("Area is: " + c);
+            return c;
+        }
+    }
+}
