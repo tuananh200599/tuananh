9742c3d4046bbac4b702fd5321f73fc29dccfc1e
tuananh200599 <anh453138@gmail.com>
2020-09-10 11:24:40 +0700

    Hello

---
+++ phuong_thuc_cdtiente/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace phuong_thuc_cdtiente
+{
+    class Program
+    {             
+        static void Main(string[] args)
+        {
+            int usd, tigia = 23000000;
+            Console.WriteLine("Nhap USD :");
+            usd = int.Parse(Console.ReadLine());
+            vnd(usd, tigia);
+            Console.ReadKey();
+        }
+        static int vnd (int a,int b)
+        {
+            int c = a * b;
+            Console.WriteLine("VNĐ là : {0} Vnđ", +c);
+            return c;
+        }
+
+    }
+}
