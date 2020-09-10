29e4dbcb3a486395fd665722a8a1fc36fe2afce0
tuananh200599 <anh453138@gmail.com>
2020-09-10 20:31:07 +0700

    Hello

---
+++ phuong_thuc_ktrasngto/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace phuong_thuc_ktrasngto
+{
+    class ktSNT
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap vao mot so bat ky n =");
+            bool kiemtra = uint.TryParse(Console.ReadLine(), out uint n);
+            while (kiemtra == false)
+            {
+                Console.Write("Du lieu khong hop le, moi nhap lai n =");
+                kiemtra = uint.TryParse(Console.ReadLine(), out n);
+            }
+            kiemtraSNT(n);
+            
+        }
+        static void kiemtraSNT(uint n)
+        {
+            if (n == 0)
+            {
+                Console.WriteLine("0 khong phai la so nguyen to");
+            }
+            for (uint i = 1; i <= n; i++)
+            {
+                if (i == n)
+                {
+                    Console.WriteLine("{0} la so nguyen to", n);
+                    break;
+                }
+                else if (n % i == 0 && i != 1)
+                {
+                    Console.WriteLine("{0} khong phai la so nguyen to", n);
+                    break;
+                }
+            }
+        }
+    }
+}
