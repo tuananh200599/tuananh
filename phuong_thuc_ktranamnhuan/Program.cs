eaa7908f25b26cb773606c6fb69f8d6aec637893
tuananh200599 <anh453138@gmail.com>
2020-09-10 11:15:27 +0700

    Hello

---
+++ phuong_thuc_ktranamnhuan/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace phuong_thuc_ktranamnhuan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int nam;
+
+            Console.Write("Nhap mot nam bat ky: ");
+            nam = Convert.ToInt32(Console.ReadLine());
+
+            namnhuan(nam);
+        }
+        static int namnhuan(int nam){
+            if ((nam % 400) == 0)
+                Console.WriteLine("La nam nhuan", +nam);
+            else if ((nam % 100) == 0)
+                Console.WriteLine(" Khong phai nam nhuan", +nam);
+            else if ((nam % 4) == 0)
+                Console.WriteLine(" La nam nhuan", +nam);
+            else
+                Console.WriteLine(" Khong phai nam nhuan.", +nam);
+
+            Console.ReadKey();
+            return nam;
+        }
+    }
+}
