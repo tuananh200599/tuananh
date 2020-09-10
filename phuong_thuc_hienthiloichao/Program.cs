6fca46aa279cd4ba80ab7891f69bf55f0f0df746
tuananh200599 <anh453138@gmail.com>
2020-09-10 11:28:41 +0700

    Heloo

---
+++ phuong_thuc_hienthiloichao/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace phuong_thuc_hienthiloichao
+{
+    class Program
+    {
+        static void chao()
+        {
+            String a;
+            Console.Write("Enter your name: ");
+            a = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Hello:" + a);
+
+        }
+        static void Main(string[] args)
+        {
+            chao();
+            Console.ReadKey();
+        }
+        } }
