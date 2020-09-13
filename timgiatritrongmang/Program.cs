331a21c4e4efd9d782ae83d99957b989260991a2
tuananh200599 <anh453138@gmail.com>
2020-09-13 18:59:32 +0700

    Hello

---
+++ timgiatritrongmang/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace timgiatritrongmang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string[] students = { "Trung", "Nam", "Anh", "Tuan", "Hoang", "Dat", "Khanh", "Long", "Linh", "Quang" };
+            
+            Console.WriteLine("Enter a name’s student:");
+            string input_name = Console.ReadLine();
+
+            
+
+            bool isExist = false;
+            for (int i = 0; i < students.Length; i++)
+            {
+                if (students[i].Equals(input_name))
+                {
+                    Console.WriteLine("Position of the students in the list " + input_name + " is: " + (i + 1));
+                    isExist = true;
+                    break;
+                }
+            }
+            if (!isExist)
+            {
+                Console.WriteLine("Not found" + input_name + " in the list.");
+            }
+        }
+    }
+}
