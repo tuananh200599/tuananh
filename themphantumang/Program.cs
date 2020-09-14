3b6c0a4857f744c1f6dbb19e197aab0f260fe67f
tuananh200599 <anh453138@gmail.com>
2020-09-14 18:12:44 +0700

    them phan tu vao mang

---
+++ themphantumang/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace themphantumang
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int[] arr = { 2, 4, 6, 8, 10 , 12 };
+            int x, k;
+            Console.WriteLine("Nhap x: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nhap vi tri can chen :");
+            k = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Mang truoc khi chen : ");
+            HienThi(arr, arr.Length - 1);
+            Chen(arr, x, k);
+            Console.WriteLine("Mang sau khi chen : ");
+            HienThi(arr, arr.Length);
+        }
+
+        private static void Chen(int[] arr, int x, int k)
+        {
+            for (int i = arr.Length - 1; i > k; i--)
+            {
+                arr[i] = arr[i - 1];
+            }
+            arr[k] = x;
+        }
+
+        static void HienThi(int[] arr, int size)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        
+    }
+}
