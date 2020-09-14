81ede94a54d239cbc3a92e3afbb61d838e21d61b
tuananh200599 <anh453138@gmail.com>
2020-09-14 10:28:45 +0700

    Hello

---
+++ lophcn/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace lophcn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          var Reactangle = new Reactangle();
+          Reactangle rect1 = new Reactangle(10,20);
+            Reactangle rect2 = new Reactangle(10,20);
+            Reactangle rect3 = new Reactangle(10,20);
+
+            var arrayRect = new Reactangle[3] {
+                new Reactangle(10,1),
+                new Reactangle(15,1),
+                new Reactangle(100,1)
+            };
+
+            arrayRect[0].height = 30;
+
+            foreach(var item in arrayRect) {
+                System.Console.WriteLine(item.GetArea());
+            }
+        }
+    }
+
+    public class Reactangle
+    {
+        public double width { get; set; }
+        public double height { get; set; }
+        public Reactangle(double _width = 1, double _height = 1)
+        {
+            width = _width;
+            height = _height;
+        }
+
+        public double GetArea()
+        {
+            return width * height;
+        }
+
+        public double GetPremeter()
+        {
+            return (width + height) * 2;
+        }
+    }
+}
