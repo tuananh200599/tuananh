35b21abcea0a900f987d62c2c4ece66a65b82f95
tuananh200599 <anh453138@gmail.com>
2020-09-15 10:50:48 +0700

    Static Method

---
+++ StaticMethod/Student.cs
@@ -0,0 +1,29 @@
+namespace StaticMethod
+{
+    public class Student
+    {
+        private int rollno;
+        private string name;
+        private static string college = "BBDIT";
+
+        //constructor to initialize the variable
+        public Student(int r, string n)
+        {
+            rollno = r;
+            name = n;
+        }
+
+        //static method to change the value of static variable
+
+        public static void Change()
+        {
+            college = "CODEGYM";
+        }
+
+        //method to display values
+        public void Display()
+        {
+            System.Console.WriteLine(rollno + " " + name + " " + college);
+        }
+    }
+}
