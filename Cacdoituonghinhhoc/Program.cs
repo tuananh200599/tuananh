6a39c9606c18232ee0d49db2deedd723b2841618
tuananh200599 <anh453138@gmail.com>
2020-09-16 17:55:11 +0700

    Cac doi tuong hinh hoc

---
+++ Cacdoituonghinhhoc/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Cacdoituonghinhhoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Shape");
+            Console.WriteLine("Nhap Color :");
+            string color = Console.ReadLine();
+            Console.WriteLine("Nhap filled (true / false):");
+            string tam = Console.ReadLine();
+            bool filled = true;
+            if (tam == "true")
+            {
+                filled = true;
+            }else if(tam == "false")
+            {
+                filled = false;
+            }         
+            Shape shape = new Shape(color, filled);
+            Console.WriteLine(shape.ToString());
+
+            Console.WriteLine("Circle");
+            Console.WriteLine("Nhap ban kinh r :");
+            double radius = double.Parse(Console.ReadLine());
+            Circle circle = new Circle(radius);
+            Console.WriteLine($"Chu vi hinh tron la {circle.getPerimeter()}");
+            Console.WriteLine($"Dien tich hinh tron la {circle.getArea()}");
+            Console.WriteLine(circle.ToString());
+
+            Console.WriteLine("Rectangle");
+            Console.WriteLine("Nhap width :");
+            double width = double.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap length :");
+            double length = double.Parse(Console.ReadLine());
+            Rectangle rectangle = new Rectangle(width,length);
+            Console.WriteLine($"Chu vi hinh tron la {rectangle.getPerimeter()}");
+            Console.WriteLine($"Dien tich hinh tron la {rectangle.getArea()}");
+            Console.WriteLine(rectangle.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Cacdoituonghinhhoc/Shape.cs
@@ -0,0 +1,52 @@
+
+namespace Cacdoituonghinhhoc
+{
+    public class Shape
+    {
+        public string color = "red";
+        public bool filled = true;
+
+        public Shape()
+        {
+
+        }
+
+        public Shape(string color, bool filled)
+        {
+            this.color = color;
+            this.filled = filled;
+        }
+
+        public string getColor()
+        {
+            return color;
+        }
+
+        public void setColor(string color)
+        {
+            this.color = color;
+        }
+
+        public bool GetFilled()
+        {
+            return filled;
+        }
+
+        public void setFilled(bool filled)
+        {
+            this.filled = filled;
+        }
+
+        public override string ToString()
+        {
+            return "A Shape with color of " + getColor() + " and " + (GetFilled() ? "filled" : "not filled");
+        }
+
+        
+
+
+
+
+    }
+}
+    
