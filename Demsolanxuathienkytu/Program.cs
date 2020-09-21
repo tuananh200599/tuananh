892caa7b2e04feb60d5b710df5b0968d91c880f4
tuananh200599 <anh453138@gmail.com>
2020-09-21 18:33:38 +0700

    Dem so lan xuat hien ky tu

---
+++ Demsolanxuathienkytu/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Demsolanxuathienkytu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            string[] s = { "Meo", "Cho", "Ho", "Ga", "Cho" };
+            Console.WriteLine("Nhap ky tu co trong mang : ");
+            string x = Console.ReadLine();
+            Console.WriteLine("So lan xuat hien cua ky tu {0} là  ", x);
+            count(s, x);
+
+        }
+        static void count(string[] z, string n)
+        {
+            int dem = 0;
+            for (int i = 0; i < z.Length; i++)
+            {
+                if (z[i] == n)
+                    dem++;
+
+            }
+            Console.WriteLine(dem);
+        }
+    }
+}
