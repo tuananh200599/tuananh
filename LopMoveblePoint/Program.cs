0add5f031885ba5c26996c0c874136ea975e4637
tuananh200599 <anh453138@gmail.com>
2020-09-22 20:17:40 +0700

    Lop Point vs Moveable Point

---
+++ LopMoveblePoint/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LopMoveblePoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Point");
+            Point point = new Point(7.8f, 5.8f);
+
+
+            Console.WriteLine(point);
+           
+
+            Console.WriteLine("MovablePoint");
+            MovablePoint movablePoint = new MovablePoint(6.4f, 8.2f);
+            Console.WriteLine(movablePoint.Move());
+            Console.WriteLine(movablePoint.ToString());
+        }
+    }
+}
