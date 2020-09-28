2238f8da51457d4e4d925046d2bc58c97dfded6c
tuananh200599 <anh453138@gmail.com>
2020-09-28 11:21:38 +0700

    Linq vs ArrayList

---
+++ Linq_ArrayList/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace Linq_ArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+            arrayList.Add(new Staff(1, "Nam", 24));
+            arrayList.Add(new Staff(2, "Kien", 21));
+            arrayList.Add(new Staff(3, "Viet", 21));
+            arrayList.Add(new Staff(4, "Hang", 23));
+            arrayList.Add(new Staff(5, "Tien", 24));
+            var arrList = arrayList.OfType<Staff>();
+            var arList = from aList in arrList
+                         where (aList.Age < 24)
+                         select aList;
+            foreach (var item in arList)
+            {
+                Console.WriteLine("ID :" + item.ID + " Name : " + item.Name + " Age : " + item.Age);
+            }
+        }
+    }
+}
---
+++ Linq_ArrayList/Staff.cs
@@ -0,0 +1,16 @@
+namespace Linq_ArrayList
+{
+    class Staff
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Staff(int id, string name, int age)
+        {
+            ID = id;
+            Name = name;
+            Age = age;
+        }
+    }
+
+}
