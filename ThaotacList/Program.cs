5e91d4b78e872a31f885a745032048d451a66fc9
tuananh200599 <anh453138@gmail.com>
2020-09-28 12:53:40 +0700

    Thao tac voi List

---
+++ ThaotacList/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace ThaotacList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            list.Add(" lion ");
+            list.Add(" cat ");
+            list.Add(" dog ");
+            list.Add(" elephant ");
+            Console.WriteLine(" Danh sach animal  : ");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item); 
+            }
+            Console.WriteLine(" Danh sach animal sau khi xoa  : ");
+            list.Remove(" dog ");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(" Danh sach animal sau khi sap xep : "); 
+            var listAnimal = from ani in list
+                         orderby ani.Length
+                         select ani;
+            foreach (var item in listAnimal)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("Index cat  : "); 
+            Console.WriteLine(list.IndexOf(" cat ")); 
+        }
+    }
+}
