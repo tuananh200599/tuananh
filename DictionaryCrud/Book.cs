8190a408af724ad7e11e7d78317e4401b4886e4b
tuananh200599 <anh453138@gmail.com>
2020-09-28 10:21:55 +0700

    Dictionary them sua xoa

---
+++ DictionaryCrud/Book.cs
@@ -0,0 +1,10 @@
+namespace DictionaryCrud
+{
+    public class Book
+    {
+        public string ISBN { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+
+    }
+}
