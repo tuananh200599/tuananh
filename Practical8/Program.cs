eb429720377d722e2c862b4d9e0e7928695d7daf
tuananh200599 <anh453138@gmail.com>
2020-09-30 11:46:20 +0700

    Practical 8

---
+++ Practical8/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practical8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program pro = new Program();
+            int choice = 0;
+
+            do
+            {
+                Console.WriteLine("1.Add a student");
+                Console.WriteLine("2.List students ");
+                Console.WriteLine("3.Classify students ");
+                Console.WriteLine("4.Exit ");
+                Console.WriteLine("Please select an item: ");
+                int.TryParse(Console.ReadLine(), out choice);
+
+                switch (choice)
+                {
+                    case 1:
+                        pro.AddStudent();
+                        break;
+
+                    case 2:
+                        pro.ListStudents();
+                        break;
+                    case 3:
+                        pro.ClassifyStudent();
+                        break;
+                    case 4:
+                        return;
+
+                }
+
+            } while (choice != 5);
+        }
+        int count = 0;
+        Dictionary<int, object> ListStudent = new Dictionary<int, object>();
+        public void AddStudent()
+        {
+            Student student = new Student();
+            student.Id = count;
+            Console.WriteLine("Name :");
+            student.Name = Console.ReadLine();
+            Console.WriteLine("Class :");
+            student.Classs = Console.ReadLine();
+            Console.WriteLine("Math mark : ");
+            student.Math = float.Parse(Console.ReadLine());
+            Console.WriteLine("Physic mark : ");
+            student.Physic = float.Parse(Console.ReadLine());
+            Console.WriteLine("Chemistry mark : ");
+            student.Chemistry = float.Parse(Console.ReadLine());
+
+
+
+            ListStudent.Add(count, student);
+            count++;
+        }
+        public void ListStudents()
+        {
+            foreach (Student item in ListStudent.Values)
+            {
+                item.Display();
+            }
+        }
+        public void ClassifyStudent()
+        {
+            foreach (Student item in ListStudent.Values)
+            {
+                item.Average();
+                item.Display();
+            }
+
+
+        }
+    }
+}
+
---
+++ Practical8/Student.cs
@@ -0,0 +1,46 @@
+using System;
+
+
+namespace Practical8
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Classs { get; set; }
+        public float Math { get; set; }
+        public float Physic { get; set; }
+        public float Chemistry { get; set; }
+
+        public Student() { }
+        public Student(int id, string name, string classs, float math, float physic, float chemistry)
+        {
+            Id = id;
+            Name = name;
+            Classs = classs;
+            this.Math = math;
+            this.Physic = physic;
+            this.Chemistry = chemistry;
+
+        }
+        public void Display()
+        {
+            float Rs = (Math + Physic + Chemistry) / 3;
+            Console.WriteLine("Student  Name : {0} Class : {1} Average : {2} ", Name, Classs, Rs);
+        }
+        public void Average()
+        {
+            float Rs = (Math + Physic + Chemistry) / 3;
+            if(Rs >= 8.5){
+                Console.WriteLine("Gud Student : ");
+            }else if (Rs >= 7){
+                Console.WriteLine("Fairs Student : ");
+            }else if (Rs >= 5){
+                Console.WriteLine("Normal Student : ");
+            }else if (Rs < 5){
+                Console.WriteLine("Bad Student : ");
+            }
+            
+        }
+    }
+}
